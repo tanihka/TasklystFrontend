3965c79f651eb48cf5afbd70dc74a8c78ab1f0dc
Shivam Tiwari <shivmmtiwari@gmail.com>
2024-08-31 00:17:50 +0530

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import { StatusBar,Text } from 'react-native';
+import { StatusBar, Text, View,StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
   return (
     <SafeAreaProvider>
       <StatusBar backgroundColor='white' barStyle='dark-content' />
-      <Text>Welcome To Tasklyst (Not Just A To Do App)</Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>Welcome To Tasklyst (Not Just A To Do App)</Text>
+      </View>
     </SafeAreaProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0', 
+  },
+  text: {
+    fontSize: 14,
+    color: '#333',
+  },
+});
 
 export default App;
